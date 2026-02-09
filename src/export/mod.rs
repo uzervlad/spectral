75d1313c290a595d14b6ddece7b4473ac41f8544
Octo <vladik0701@gmail.com>
2026-02-09 18:08:21 +0300

    separate export process to another file

--- src/export/mod.rs
+++ src/export/mod.rs
@@ -1,11 +1,12 @@
 use std::fmt::Display;
 use std::fs::File;
-use std::io::Write;
 
 use eyre::Result;
 use rfd::FileDialog;
 
 use crate::timing::TimingPoint;
+
+mod osu;
 
 trait ApplyExportFormat {
 	fn apply_format(self, fmt: ExportFormat) -> Self;
@@ -39,22 +40,10 @@ impl ExportFormat {
 		]
 	}
 
-	fn run(self, mut file: File, timing_points: &[TimingPoint]) -> Result<()> {
-		writeln!(file, "osu file format v14")?;
-		writeln!(file)?;
-		writeln!(file, "[TimingPoints]")?;
-
-		for tp in timing_points {
-			writeln!(
-				file,
-				"{:.0},{:.8},{},2,1,100,1,0",
-				tp.offset,
-				tp.ms_per_beat(),
-				tp.signature.0
-			)?;
+	fn run(self, file: File, timing_points: &[TimingPoint]) -> Result<()> {
+		match self {
+			Self::Osu => osu::export(file, timing_points)
 		}
-
-		Ok(())
 	}
 }
 
---
+++ src/export/osu.rs
@@ -0,0 +1,24 @@
+use std::io::Write;
+use std::fs::File;
+
+use eyre::Result;
+
+use crate::timing::TimingPoint;
+
+pub fn export(mut file: File, timing_points: &[TimingPoint]) -> Result<()> {
+	writeln!(file, "osu file format v14")?;
+	writeln!(file)?;
+	writeln!(file, "[TimingPoints]")?;
+
+	for tp in timing_points {
+		writeln!(
+			file,
+			"{:.0},{:.8},{},2,1,100,1,0",
+			tp.offset,
+			tp.ms_per_beat(),
+			tp.signature.0
+		)?;
+	}
+
+	Ok(())
+}
