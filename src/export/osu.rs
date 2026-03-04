ece620f0101684a2f85f6d2ebf5a589f46c0cdae
Octo <vladik0701@gmail.com>
2026-03-04 16:17:07 +0300

    update existing .osu exports

--- src/export/osu.rs
+++ src/export/osu.rs
@@ -1,3 +1,4 @@
+use std::fmt::Write as _;
 use std::fs::File;
 use std::io::Write as _;
 
@@ -25,25 +26,34 @@ pub fn create(mut file: File, timing_points: &[TimingPoint]) -> Result<()> {
 
 pub fn patch(mut file: File, contents: String, timing_points: &[TimingPoint]) -> Result<()> {
 	let mut in_timing = false;
+	let mut done = false;
+
+	let mut section = "[TimingPoints]\n".to_owned();
+
+	for tp in timing_points {
+		writeln!(
+			section,
+			"{:.0},{:.8},{},2,1,100,1,0",
+			tp.offset,
+			tp.ms_per_beat(),
+			tp.signature.0
+		)?;
+	}
 
 	for line in contents.lines() {
 		if line.trim() == "[TimingPoints]" {
-			writeln!(file, "{}", line)?;
-
-			for tp in timing_points {
-				writeln!(
-					file,
-					"{:.0},{:.8},{},2,1,100,1,0",
-					tp.offset,
-					tp.ms_per_beat(),
-					tp.signature.0
-				)?;
-			}
-
+			writeln!(file, "{}", section)?;
 			writeln!(file)?;
 
 			in_timing = true;
+			done = true;
 			continue;
+		} else if line.trim() == "[HitObjects]" && !done {
+			// New .osu files without timing points don't have
+			// a [TimingPoints] section, so we insert one
+			// before [HitObjects]
+			writeln!(file, "{}", section)?;
+			writeln!(file)?;
 		}
 
 		if in_timing && line.starts_with('[') {
